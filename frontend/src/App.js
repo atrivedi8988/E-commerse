f12669c9bde58310a83b484f638d20b5b818588b
atrivedi8988 <atrivedi8988@gmail.com>
2023-01-28 20:11:31 +0530

    header and footer work complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import Header from './Components/layout/Header.js'
-import './App.css';
+import Header from "./Components/layout/Header/Header";
+import Footer from "./Components/layout/Footer/Footer"
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Webfont from "webfontloader"
+import React from "react";
 
 function App() {
+
+  React.useEffect(()=>{
+    Webfont.load({
+      google:{
+        families:["Roboto","Droid Sans","Chilanka"]
+      }
+    })
+  },[])
   return (
-    <Header/>
+    <Router>
+      <Header />
+      <Footer/>
+    </Router>
   );
 }
 
